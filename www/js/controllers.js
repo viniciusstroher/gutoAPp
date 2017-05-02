c172b9df062b2ffc0bbcd29ab7dbbb08afd51ae0
vinicius~~ <viniciusferreirwk@gmail.com>
2017-05-01 21:19:10 -0300

    fix luminosidade

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,12 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$http,$ionicLoading) {
+.controller('DashCtrl', function($scope,$http,$ionicLoading,$timeout) {
     $scope.dados = {};
     $scope.dadosInvalidos = true;
     var url = 'localhost';
     
+
+
     $scope.atualizaDados = function() {
       $http({
         method: 'GET',
@@ -13,12 +15,29 @@ angular.module('starter.controllers', [])
           console.log(response);
           $scope.dados = response.data;
           $scope.dadosInvalidos = false
+
+          if($scope.dados.luminosidade < 200){
+            $scope.ligaRele1();
+          }else{
+            $scope.desligaRele1();
+          }
+
       }, function errorCallback(response) {
           console.log(response);    
       });
       $scope.$broadcast('scroll.refreshComplete');
     }
-    $scope.atualizaDados();
+    //$scope.atualizaDados();
+
+
+    $scope.atualizaSilent = function(){
+      $scope.atualizaDados();
+      $timeout(function(){
+        $scope.atualizaSilent();
+      },10000);
+
+    }
+    $scope.atualizaSilent();
 
 
     $scope.ligaRele1 = function(){
